a391f93018dd06860a48730b846113fc006731c6
Luce Michaelis <lucerorocks17@gmail.com>
2017-01-18 22:04:59 +0000

    made a square

---
+++ script.js
@@ -0,0 +1,22 @@
+   TinyTurtle.apply(window); 
+   stamp();
+   forward(30);
+   left(90);
+   forward(60);
+   left(90);
+  forward(30);
+  left(90);
+  forward(60);
+
+
+forward(30);
+right(90);
+penUp();
+forward(30);
+forward(30);
+right(90);
+forward(65);
+right(90);
+forward(90);
+penDown();
+stamp();
